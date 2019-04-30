3a148768c28657a70809ea140c7036db79c16893
vishnu <tg.vishnu@gmail.com>
2019-05-01 02:36:27 +0530

    Insertion sort algorithm added

--- Src/Vishnu.Extensions/Sorting/Core/InsertionSort.cs
+++ Src/Vishnu.Extensions/Sorting/Core/InsertionSort.cs
@@ -4,22 +4,39 @@ using System.Text;
 
 namespace Vishnu.Extensions.Sorting.Core
 {
+    /// <summary>
+    /// Insertion sort is a simple sorting algorithm that works the way we sort playing cards in our hands.
+    /// Algorithm
+    /// Sort an arr[] of size n
+    /// insertionSort(arr, n)
+    /// Loop from i = 1 to n-1.
+    /// ……a) Pick element arr[i] and insert it into sorted sequence arr[0…i - 1]
+    /// </summary>
+    /// <typeparam name="T">Type of input</typeparam>
     public class InsertionSort<T> : ISortingAlgorithm<T>
     {
         private readonly IComparer<T> _comparer = null;
 
+        /// <summary>
+        /// Creates new instance of <see cref="InsertionSort{T}"/> class
+        /// </summary>
+        /// <param name="comparer"><see cref="IComparer{T}"/></param>
         public InsertionSort(IComparer<T> comparer)
         {
             _comparer = comparer;
         }
 
+        /// <summary>
+        /// Sorts input in ascending order
+        /// </summary>
+        /// <param name="input">input</param>
         public virtual void Sort(T[] input)
         {
-            for(int ii=1; ii<input.Length; ii++)
+            for (int ii = 1; ii < input.Length; ii++)
             {
                 var key = input[ii];
                 var jj = ii - 1;
-                while(jj >=0 && (_comparer.Compare(input[jj], key) > 0))
+                while (jj >= 0 && (_comparer.Compare(input[jj], key) > 0))
                 {
                     input[jj + 1] = input[jj];
                     jj = jj - 1;
---
+++ Src/Vishnu.Extensions/Sorting/Extensions/InsertionSortExtension.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Vishnu.Extensions.Sorting.Core;
+using Vishnu.Extensions.Sorting.Helpers;
+
+namespace Vishnu.Extension.Sorting
+{
+    /// <summary>
+    /// Extension methods for Insertion sort
+    /// </summary>
+    public static class InsertionSortExtension
+    {
+        /// <summary>
+        /// Sorts input in ascending order using Insertion sort technique
+        /// </summary>
+        /// <typeparam name="T">Type of data</typeparam>
+        /// <param name="sort">ISort</param>
+        /// <param name="input">input data</param>
+        /// <param name="comparer">Comparer must returns '1' if the first element is greater than next</param>
+        /// <returns>The ascending ordered content</returns>
+        public static T[] UseInsertion<T>(this ISort sort, T[] input, IComparer<T> comparer)
+        {
+            return sort.Sort<T>(SortingTypes.Insertion, input, comparer);
+        }
+
+        /// <summary>
+        /// Sorts input in ascending order using Insertion sort technique
+        /// </summary>
+        /// <typeparam name="T">Type of data</typeparam>
+        /// <param name="sort">ISort</param>
+        /// <param name="sortingAlgorithmFactory">Sorting algorithm factory</param>
+        /// <param name="input">input data</param>
+        /// <param name="comparer">Comparer must returns '1' if the first element is greater than next</param>
+        /// <returns>The ascending ordered content</returns>
+        public static T[] UseInsertion<T>(this ISort sort, ISortingAlgorithmFactory sortingAlgorithmFactory, T[] input, IComparer<T> comparer)
+        {
+            return sort.Sort<T>(sortingAlgorithmFactory, SortingTypes.Insertion, input, comparer);
+        }
+
+        /// <summary>
+        /// Sorts input in ascending order using Insertion sort technique
+        /// </summary>
+        /// <param name="sort">ISort</param>
+        /// <param name="input">input data</param>
+        /// <param name="comparer">Comparer must returns '1' if the first element is greater than next</param>
+        /// <returns>The ascending ordered content</returns>
+        public static string UseInsertion(this ISort sort, string input, IComparer<char> comparer = null)
+        {
+            return sort.Sort(SortingTypes.Insertion, input, comparer);
+        }
+
+        /// <summary>
+        /// Sorts input in ascending order using Insertion sort technique
+        /// </summary>
+        /// <param name="sort">ISort</param>
+        /// <param name="sortingAlgorithmFactory">Sorting algorithm factory</param>
+        /// <param name="input">input data</param>
+        /// <param name="comparer">Comparer must returns '1' if the first element is greater than next</param>
+        /// <returns>The ascending ordered content</returns>
+        public static string UseInsertion(this ISort sort, ISortingAlgorithmFactory sortingAlgorithmFactory, string input, IComparer<char> comparer = null)
+        {
+            return sort.Sort(sortingAlgorithmFactory, SortingTypes.Insertion, input, comparer);
+        }
+    }
+}
